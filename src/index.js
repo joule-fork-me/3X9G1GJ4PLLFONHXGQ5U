d76eb5f3be1812026ebabacc213d8d5c92965f11
Jaisen Mathai <jaisen@jmathai.com>
2018-03-01 01:52:50 -0800

    working

--- src/index.js
+++ src/index.js
@@ -11,17 +11,42 @@
  *    event.{queryStringParam}, Query string parameters as defined in your .joule.yml
  */
 var Response = require('joule-node-response');
+var {google} = require('googleapis');
+var scopes = [
+      'https://www.googleapis.com/auth/admin.directory.user',
+      'https://www.googleapis.com/auth/admin.directory.group'
+    ];
+const authClient = new google.auth.JWT(
+  process.env.CLIENT_EMAIL,
+  null,
+  process.env.PRIVATE_KEY,
+  scopes,
+  process.env.IMPERSONATE_EMAIL
+);
+const admin = google.admin('directory_v1');
 
 exports.handler = function(event, context) {
 	var response = new Response();
 	response.setContext(context);
 
-  var name = event.query['name'] || 'World';
-  var greeting = 'Hello, ' + name + '.';
+  authClient.authorize(function(err, data) {
+    if (err) {
+      throw err;
+    }
+    console.log('YOU ARE AUTHORIZED');
+    
+    const memberParams = {groupKey:"j-test-3@shelterplus.in", auth: authClient};
+    admin.groups.get(memberParams, function(err, data) {
+      if (err) {
+        console.log(err);
+        //throw err;
+      }
+      var result = {
+        "group": data.data
+      };
+      
+      response.send(result);
+    });
+  });
 
-  var result = {
-    "message": greeting
-  };
-  
-  response.send(result);
 };
