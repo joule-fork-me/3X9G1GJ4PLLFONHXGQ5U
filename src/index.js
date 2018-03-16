ef657456c6e1c5a9dd2e4a4a47bdb042d206e2e1
Jaisen Mathai <jaisen@jmathai.com>
2018-03-16 00:03:26 -0700

    Add dynamic_group function

--- src/index.js
+++ src/index.js
@@ -11,10 +11,10 @@
  *    event.{queryStringParam}, Query string parameters as defined in your .joule.yml
  */
 var Response = require('joule-node-response');
-var google = require('googleapis').google;
+var {google} = require('googleapis');
 var scopes = [
-      'https://www.googleapis.com/auth/admin.directory.user',
-      'https://www.googleapis.com/auth/admin.directory.group'
+      'https://www.googleapis.com/auth/admin.directory.user'
+      , 'https://www.googleapis.com/auth/admin.directory.group'
     ];
 
 const authClient = new google.auth.JWT(
@@ -26,9 +26,53 @@ const authClient = new google.auth.JWT(
 );
 const admin = google.admin('directory_v1');
 
-exports.handler = function(event, context) {
+var watch_users = function(event, context) {
 	var response = new Response();
   var groupKey = event.query['groupKey'] || '';
+	response.setContext(context);
+
+  authClient.authorize(function(err, data) {
+    if (err) {
+      console.log("AUTH FAILED");
+			console.log(err);
+      response.send(err.response.data);
+      return;
+    }
+    
+    const watchParams = {
+      event: "update"
+      , viewType: "admin_view"
+      , customer: "my_customer"
+    };
+    const watchOptions = {
+      id: '' + parseInt(Math.random()*1000000000)
+      , auth: authClient
+      , address: "https://dg.jmathai.com/jmathai/d-groups-syn-2"
+      , token: '' + parseInt(Math.random()*1000000000)
+      , type: "web_hook"
+      , payload: true
+    };
+    //const params = Object.assign(watchParams, watchOptions);
+    admin.users.watch(watchParams, watchOptions, function(err, data) {
+      if (err) {
+        console.log("WATCH FAILED");
+				console.log(err.response.data);
+				console.log(err.response.data.error.errors);
+        response.send(err.response.data);
+        return;
+      }
+      var result = {
+        "watch": JSON.stringify(data.data)
+      };
+      
+      response.send(result);
+    });
+  });
+};
+
+var get_group_by_key = function(event, context) {
+	var response = new Response();
+  var groupKey = event.query['groupKey'] || 'jaisen-test@shelterplus.in';
 	response.setContext(context);
 
   authClient.authorize(function(err, data) {
@@ -54,3 +98,15 @@ exports.handler = function(event, context) {
   });
 
 };
+
+var dynamic_group = function(event, context) {
+  console.log(event);
+	var response = new Response();
+	response.setContext(context);
+  response.send(event);
+};
+
+exports.handler = dynamic_group;
+// exports.handler = watch_users;
+// exports.handler = get_group_by_key;
+
