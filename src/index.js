263d2320d3a6ff8f04e0d3e01059d6dde202b1c3
Jaisen Mathai <jaisen@jmathai.com>
2018-03-16 23:29:12 -0700

    add api and clean up

--- src/index.js
+++ src/index.js
@@ -10,98 +10,72 @@
  *    event.{pathParam}, Path parameters as defined in your .joule.yml
  *    event.{queryStringParam}, Query string parameters as defined in your .joule.yml
  */
-var Response = require('joule-node-response');
-var {google} = require('googleapis');
-var scopes = [
+var Response = require('joule-node-response')
+    , JouleNodeDatabase = require('joule-node-database')
+    , myDb = new JouleNodeDatabase()
+    , {google} = require('googleapis')
+    , scopes = [
       'https://www.googleapis.com/auth/admin.directory.user'
       , 'https://www.googleapis.com/auth/admin.directory.group'
     ];
 
 const authClient = new google.auth.JWT(
-  process.env.CLIENT_EMAIL,
-  null,
-  process.env.PRIVATE_KEY,
-  scopes,
-  process.env.IMPERSONATE_EMAIL
-);
-const admin = google.admin('directory_v1');
+        process.env.CLIENT_EMAIL,
+        null,
+        process.env.PRIVATE_KEY,
+        scopes,
+        process.env.IMPERSONATE_EMAIL
+      )
+      , admin = google.admin('directory_v1');
 
-var watch_users = function(event, context) {
-	var response = new Response();
-  var groupKey = event.query['groupKey'] || '';
+var handler = function(event, context) {
+	var response = new Response()
+      , pathArray = event.path
+      , httpMethod = event.httpMethod;
 	response.setContext(context);
 
-  authClient.authorize(function(err, data) {
-    if (err) {
-      console.log("AUTH FAILED");
-			console.log(err);
-      response.send(err.response.data);
-      return;
+  if(pathArray.length === 0) {
+    // base path is the webhook
+    webhook(event, context, response);
+  } else {
+    switch(pathArray[0]) {
+      case 'api':
+        switch(httpMethod) {
+          case 'GET':
+            api_get(event, context, response);
+            break;
+          case 'POST':
+            api_post(event, context, response);
+            break;
+        }
+        break;
     }
-    
-    const watchParams = {
-      event: "update"
-      , viewType: "admin_view"
-      , customer: "my_customer"
-    };
-    const watchOptions = {
-      id: '' + parseInt(Math.random()*1000000000)
-      , auth: authClient
-      , address: "https://dg.jmathai.com/jmathai/d-groups-syn-2"
-      , token: '' + parseInt(Math.random()*1000000000)
-      , type: "web_hook"
-      , payload: true
-    };
-    //const params = Object.assign(watchParams, watchOptions);
-    admin.users.watch(watchParams, watchOptions, function(err, data) {
-      if (err) {
-        console.log("WATCH FAILED");
-				console.log(err.response.data);
-				console.log(err.response.data.error.errors);
-        response.send(err.response.data);
-        return;
-      }
-      var result = {
-        "watch": JSON.stringify(data.data)
-      };
-      
-      response.send(result);
-    });
+  }
+};
+
+var api_get = function(event, context, response) {
+  myDb.get('groups').done(function(err, data) {
+    if(err) {
+      console.log(err);
+      response.send(err);
+    }
+    console.log(data);
+    response.send('hi');
   });
 };
 
-var get_group_by_key = function(event, context) {
-	var response = new Response();
-  var groupKey = event.query['groupKey'] || 'jaisen-test@shelterplus.in';
-	response.setContext(context);
-
-  authClient.authorize(function(err, data) {
-    if (err) {
-			console.log(err);
-      response.send(err.response.data);
-      return;
+var api_post = function(event, context, response) {
+  myDb.set('groups', {foo: 'bar'}).done(function(err, data) {
+    if(err) {
+      console.log(err);
+      response.send(err);
     }
-    
-    const memberParams = {groupKey:groupKey, auth: authClient};
-    admin.groups.get(memberParams, function(err, data) {
-      if (err) {
-				console.log(err);
-        response.send(err.response.data);
-        return;
-      }
-      var result = {
-        "group": data.data
-      };
-      
-      response.send(result);
-    });
+    console.log(data);
+    response.send('hi');
   });
-
 };
 
-var dynamic_group = function(event, context) {
-	var response = new Response();
-	response.setContext(context);
+var webhook = function(event, context, response) {
   authClient.authorize(function(err, data) {
     if (err) {
 			console.log(err);
@@ -158,29 +132,80 @@ var dynamic_group = function(event, context) {
   });
 };
 
-var add_to_group = function(admin, user) {
-  const resource = Object.assign(user, {role: 'MEMBER'});
-  const memberParams = {groupKey: 'dynamic-group-of-jaisens@shelterplus.in', resource: resource};
+exports.handler = handler;
+
+// exports.handler = watch_users;
+// exports.handler = get_group_by_key;
+//
+/*
+var get_group_by_key = function(event, context) {
+	var response = new Response();
+  var groupKey = event.query['groupKey'] || 'jaisen-test@shelterplus.in';
+	response.setContext(context);
+
   authClient.authorize(function(err, data) {
     if (err) {
 			console.log(err);
       response.send(err.response.data);
       return;
     }
-    admin.members.insert(memberParams, function(err, data) {
+    
+    const memberParams = {groupKey:groupKey, auth: authClient};
+    admin.groups.get(memberParams, function(err, data) {
       if (err) {
-        console.log('add_to_group err');
-        console.log(err);
+				console.log(err);
+        response.send(err.response.data);
         return;
       }
-      console.log('add_to_group success');
-      console.log(data);
-      return;
-
+      var result = {
+        "group": data.data
+      };
+      
+      response.send(result);
     });
   });
 };
+*/
+/*var watch_users = function(event, context) {
+	var response = new Response();
+  var groupKey = event.query['groupKey'] || '';
+	response.setContext(context);
 
-exports.handler = dynamic_group;
-// exports.handler = watch_users;
-// exports.handler = get_group_by_key;
+  authClient.authorize(function(err, data) {
+    if (err) {
+      console.log("AUTH FAILED");
+			console.log(err);
+      response.send(err.response.data);
+      return;
+    }
+    
+    const watchParams = {
+      event: "update"
+      , viewType: "admin_view"
+      , customer: "my_customer"
+    };
+    const watchOptions = {
+      id: '' + parseInt(Math.random()*1000000000)
+      , auth: authClient
+      , address: event.base_url
+      , token: '' + parseInt(Math.random()*1000000000)
+      , type: "web_hook"
+      , payload: true
+    };
+    //const params = Object.assign(watchParams, watchOptions);
+    admin.users.watch(watchParams, watchOptions, function(err, data) {
+      if (err) {
+        console.log("WATCH FAILED");
+				console.log(err.response.data);
+				console.log(err.response.data.error.errors);
+        response.send(err.response.data);
+        return;
+      }
+      var result = {
+        "watch": JSON.stringify(data.data)
+      };
+      
+      response.send(result);
+    });
+  });
+};*/
